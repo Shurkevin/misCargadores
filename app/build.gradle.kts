ec008aea46ebe5854722e69e1882d3e5f86db196
Kevin Moroder <origds@Orianas-MacBook-Pro.local>
2026-09-03 23:02:04 +0200

    fix: Upgrading version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "es.origds.cargadoresmercadona"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "0.3.1"
         manifestPlaceholders["appAuthRedirectScheme"] = "rv"
     }
