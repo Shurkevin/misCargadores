ee0a05ecd0f88108eae6f634e8901037b2cc26dc
Kevin Moroder <origds@Orianas-MacBook-Pro.local>
2026-09-03 22:56:29 +0200

    fix: We upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 2
-        versionName = "0.3.0"
+        versionName = "0.3.1"
         manifestPlaceholders["appAuthRedirectScheme"] = "rv"
     }
 
