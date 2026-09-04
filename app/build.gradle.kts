3c3d912221f0c3deff2b6f2b1ff7932ba9bb6bad
Kevin Moroder <origds@Orianas-MacBook-Pro.local>
2026-09-04 17:06:34 +0200

    feat: New release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "es.origds.cargadoresmercadona"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.1"
+        versionCode = 4
+        versionName = "0.3.2"
         manifestPlaceholders["appAuthRedirectScheme"] = "rv"
     }
 
